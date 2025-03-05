4f3cabdf1dadff21ef2fa6553ce4f049147507c9
Alex Fong <110338657+Chama1026@users.noreply.github.com>
2025-03-05 22:54:25 +0800

    Update Customer.sql

--- SQL query/Customer.sql
+++ SQL query/Customer.sql
@@ -1,37 +1,36 @@
-WITH PurchaseDate AS (
-	SELECT
-		CustomerID, 
-		MIN(OrderDate) AS [First Purchase Date],
-		Max(OrderDate) AS [Last Purchase Date]
-	FROM
-		Sales.SalesOrderHeader
-	GROUP BY
-		CustomerID
-)
-SELECT DISTINCT
-	c.CustomerID,
-	CASE
-		WHEN c.StoreID IS NOT NULL THEN s.Name
-		ELSE CONCAT(p.FirstName, ISNULL(' ' + p.MiddleName, ''), ' ',p.LastName) 
-	END AS [Customer Name],
-	CASE
-		WHEN c.StoreID IS NOT NULL THEN 'Business'
-		ELSE 'Individual'
-	END AS [Customer Type],
-	a.City,
-	CASE	
-		WHEN p.EmailPromotion = 0 THEN 'Non-subscriber'
-		ELSE 'Subscriber'
-	END AS [Email Promotion],
-	pd.[First Purchase Date],
-	pd.[Last Purchase Date]
-FROM 
-	Sales.Customer c
-	LEFT JOIN Person.Person p ON c.CustomerID = p.BusinessEntityID
-	LEFT JOIN Person.BusinessEntityAddress ba ON c.CustomerID = ba.BusinessEntityID
-	LEFT JOIN Person.Address a ON ba.AddressID = a.AddressID
-	LEFT JOIN PurchaseDate pd ON c.CustomerID = pd.CustomerID
-	LEFT JOIN Sales.Store s ON c.StoreID = s.BusinessEntityID
-WHERE
-	pd.[First Purchase Date] IS NOT NULL
+WITH purchasedate
+     AS (SELECT customerid,
+                Min(orderdate) AS [First Purchase Date],
+                Max(orderdate) AS [Last Purchase Date]
+         FROM   sales.salesorderheader
+         GROUP  BY customerid)
+SELECT DISTINCT c.customerid,
+                CASE
+                  WHEN c.storeid IS NOT NULL THEN s.NAME
+                  ELSE Concat(p.firstname, Isnull(' ' + p.middlename, ''), ' ',
+                       p.lastname)
+                END AS [Customer Name],
+                CASE
+                  WHEN c.storeid IS NOT NULL THEN 'Business'
+                  ELSE 'Individual'
+                END AS [Customer Type],
+                a.city,
+                CASE
+                  WHEN p.emailpromotion = 0 THEN 'Non-subscriber'
+                  ELSE 'Subscriber'
+                END AS [Email Promotion],
+                pd.[first purchase date],
+                pd.[last purchase date]
+FROM   sales.customer c
+       LEFT JOIN person.person p
+              ON c.customerid = p.businessentityid
+       LEFT JOIN person.businessentityaddress ba
+              ON c.customerid = ba.businessentityid
+       LEFT JOIN person.address a
+              ON ba.addressid = a.addressid
+       LEFT JOIN purchasedate pd
+              ON c.customerid = pd.customerid
+       LEFT JOIN sales.store s
+              ON c.storeid = s.businessentityid
+WHERE  pd.[first purchase date] IS NOT NULL 
 
